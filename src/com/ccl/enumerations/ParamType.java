b136b8d2f4efa89dded9660cb1a20038dfedc2f2
Arpaesis <arpaesis@gmail.com>
2019-03-03 02:29:06 -0500

    Removed unused ParamType value.

--- src/com/ccl/enumerations/ParamType.java
+++ src/com/ccl/enumerations/ParamType.java
@@ -2,5 +2,5 @@ package com.ccl.enumerations;
 
 public enum ParamType
 {
-	BOOLEAN, BYTE, CHAR, DOUBLE, FLOAT, INT, LONG, SHORT, STRING, WILDCARD;
+	BOOLEAN, BYTE, CHAR, DOUBLE, FLOAT, INT, LONG, SHORT, STRING;
 }
