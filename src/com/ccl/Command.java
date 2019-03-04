36103fe9ae27676b33231e3a1cb3ed2d81654789
Arpaesis <arpaesis@gmail.com>
2019-03-03 20:32:38 -0500

    Moved the parser to its own class.

--- src/com/ccl/Command.java
+++ src/com/ccl/Command.java
@@ -1,28 +1,24 @@
 package com.ccl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.ccl.args.Argument;
 import com.ccl.args.Arguments;
 import com.ccl.args.OptionalArgument;
-import com.ccl.args.ProcessedArgument;
 import com.ccl.args.RequiredArgument;
 import com.ccl.enumerations.Result;
 import com.ccl.schedule.Task;
-import com.ccl.utils.MathUtils;
-import com.ccl.utils.StringUtils;
+import com.ccl.utils.Parser;
 
 public abstract class Command<T, R>
 {
 
 	public final List<Argument> parameters = new ArrayList<>();
-	int reqArgCount = 0;
-	int optArgCount = 0;
+	private int reqArgCount = 0;
+	private int optArgCount = 0;
 
 	private String name = "";
 	private String help = "";
@@ -45,6 +41,8 @@ public abstract class Command<T, R>
 	private boolean isDisabled = false;
 
 	private CommandManager<T, R> manager;
+
+	private Parser<T, R> parser = new Parser<>();
 
 	public Command()
 	{
@@ -179,7 +177,7 @@ public abstract class Command<T, R>
 
 	}
 
-	private void shutdown(T obj, Result result, String response)
+	public void shutdown(T obj, Result result, String response)
 	{
 		this.result(obj, result, response);
 		this.result(result, response);
@@ -223,240 +221,7 @@ public abstract class Command<T, R>
 
 	private Arguments processInput(T obj, String input)
 	{
-
-		List<String> list = new ArrayList<>();
-		Matcher m = stringPattern.matcher(input);
-		while (m.find())
-			list.add(m.group(1));
-
-		String[] tempArr = new String[list.size()];
-		String[] preArgs = list.toArray(tempArr);
-
-		String[] rawArgs = Arrays.copyOfRange(preArgs, 1, preArgs.length);
-
-		List<ProcessedArgument<?>> arguments = new ArrayList<>();
-
-		for (int i = 0; i < rawArgs.length; i++)
-		{
-			Matcher tm = numberPattern.matcher(rawArgs[i]);
-
-			if (!this.shouldExecute)
-			{
-				break;
-			}
-			else if (rawArgs.length < parameters.size() - this.optArgCount || rawArgs.length > this.parameters.size())
-			{
-				this.shutdown(obj, Result.FAILURE, "The command has an invalid number of parameters!");
-				break;
-			}
-
-			switch (this.parameters.get(i).getType())
-			{
-			case BOOLEAN:
-				if (rawArgs[i].equals("true") || rawArgs[i].equals("false") || rawArgs[i].equals("1") || rawArgs[i].equals("0"))
-				{
-					if (rawArgs[i].equals("1"))
-					{
-						rawArgs[i] = "true";
-					}
-					else if (rawArgs[i].equals("0"))
-					{
-						rawArgs[i] = "false";
-					}
-					arguments.add(new ProcessedArgument<Boolean>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Boolean.parseBoolean(rawArgs[i])));
-				}
-				else
-				{
-					this.shutdown(obj, Result.FAILURE, "Failed to parse argument " + rawArgs[i] + ", expected a boolean!");
-				}
-				break;
-			case BYTE:
-				if (tm.find() && !rawArgs[i].contains(":"))
-				{
-					byte bValue = Byte.parseByte(rawArgs[i]);
-					rawArgs[i] = MathUtils.clampb(bValue, parameters.get(i));
-					arguments.add(new ProcessedArgument<Byte>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Byte.parseByte(rawArgs[i])));
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					byte bValue = Byte.parseByte(split[1]);
-					arguments.add(new ProcessedArgument<Byte>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, bValue));
-				}
-				break;
-			case CHAR:
-				if (rawArgs[i].length() >= 2)
-				{
-					this.shutdown(obj, Result.FAILURE, "Failed to parse argument " + rawArgs[i] + ", expected a single character!");
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					char cValue = split[1].charAt(0);
-					arguments.add(new ProcessedArgument<Character>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, cValue));
-				}
-				else
-				{
-					arguments.add(new ProcessedArgument<Character>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], rawArgs[i].charAt(0)));
-				}
-				break;
-			case DOUBLE:
-				if (tm.find() && !rawArgs[i].contains(":"))
-				{
-					double dValue = Double.parseDouble(rawArgs[i]);
-					rawArgs[i] = MathUtils.clampd(dValue, parameters.get(i));
-					arguments.add(new ProcessedArgument<Double>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Double.parseDouble(rawArgs[i])));
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					double dValue = Double.parseDouble(split[1]);
-					arguments.add(new ProcessedArgument<Double>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, dValue));
-				}
-				break;
-			case FLOAT:
-				if (tm.find() && !rawArgs[i].contains(":"))
-				{
-					float fValue = Float.parseFloat(rawArgs[i]);
-					rawArgs[i] = MathUtils.clampf(fValue, parameters.get(i));
-					arguments.add(new ProcessedArgument<Float>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Float.parseFloat(rawArgs[i])));
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					float fValue = Float.parseFloat(split[1]);
-					arguments.add(new ProcessedArgument<Float>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, fValue));
-				}
-				break;
-			case INT:
-
-				if (tm.find() && !rawArgs[i].contains(":"))
-				{
-					int iValue = 0;
-
-					boolean neg = false;
-
-					rawArgs[i] = rawArgs[i].replaceAll("_", "").replaceAll(",", "");
-
-					if (rawArgs[i].startsWith("-"))
-					{
-						neg = true;
-						rawArgs[i] = rawArgs[i].substring(1);
-					}
-
-					if (rawArgs[i].startsWith("+"))
-					{
-						rawArgs[i] = rawArgs[i].substring(1);
-					}
-
-					if (rawArgs[i].startsWith("0b"))
-					{
-						iValue = Integer.parseInt(rawArgs[i].replace("0b", ""), 2);
-					}
-					else if (rawArgs[i].startsWith("0x"))
-					{
-						iValue = Integer.parseInt(rawArgs[i].replace("0x", ""), 16);
-					}
-					else if (rawArgs[i].startsWith("0") && rawArgs[i].length() != 1)
-					{
-						iValue = Integer.parseInt(rawArgs[i].substring(1), 8);
-					}
-					else
-					{
-						iValue = Integer.parseInt(rawArgs[i]);
-					}
-
-					if (neg)
-					{
-						iValue = 0 - iValue;
-					}
-
-					rawArgs[i] = MathUtils.clampi(iValue, parameters.get(i));
-					arguments.add(new ProcessedArgument<Integer>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Integer.parseInt(rawArgs[i])));
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					int iValue = Integer.parseInt(split[1]);
-					arguments.add(new ProcessedArgument<Integer>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, iValue));
-				}
-				else
-				{
-					this.shutdown(obj, Result.FAILURE, "Given parameter (" + rawArgs[i] + ") is not a valid integer value!");
-				}
-				break;
-			case LONG:
-				if (tm.find() && !rawArgs[i].contains(":"))
-				{
-					long lValue = Long.parseLong(rawArgs[i]);
-					rawArgs[i] = MathUtils.clampl(lValue, parameters.get(i));
-					arguments.add(new ProcessedArgument<Long>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Long.parseLong(rawArgs[i])));
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					long lValue = Long.parseLong(split[1]);
-					arguments.add(new ProcessedArgument<Long>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, lValue));
-				}
-				break;
-			case SHORT:
-				if (tm.find() && !rawArgs[i].contains(":"))
-				{
-					short sValue = Short.parseShort(rawArgs[i]);
-					rawArgs[i] = MathUtils.clamps(sValue, parameters.get(i));
-					arguments.add(new ProcessedArgument<Short>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], Short.parseShort(rawArgs[i])));
-				}
-				else if (rawArgs[i].contains(":"))
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					short sValue = Short.parseShort(split[1]);
-					arguments.add(new ProcessedArgument<Short>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, sValue));
-				}
-				break;
-			case STRING: // TODO: Make this not ass.
-
-				if (!rawArgs[i].contains(":"))
-				{
-					if (rawArgs[i].startsWith("\""))
-					{
-						rawArgs[i] = rawArgs[i].substring(1);
-					}
-
-					if (rawArgs[i].endsWith("\""))
-					{
-						rawArgs[i] = StringUtils.removeLastCharOptional(rawArgs[i]);
-					}
-					arguments.add(new ProcessedArgument<String>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), rawArgs[i], rawArgs[i]));
-				}
-				else
-				{
-					String[] split = rawArgs[i].split(":");
-					String tag = split[0];
-					String content = split[1];
-					if (content.startsWith("\""))
-					{
-						content = content.substring(1);
-					}
-
-					if (content.endsWith("\""))
-					{
-						content = StringUtils.removeLastCharOptional(content);
-					}
-					arguments.add(new ProcessedArgument<String>(this.parameters.get(i).getArgName(), this.parameters.get(i).getType(), tag, content));
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		return new Arguments(arguments);
+		return this.parser.processInput(this, obj, input);
 	}
 
 	public CommandManager<T, R> getCommandManager()
@@ -497,5 +262,25 @@ public abstract class Command<T, R>
 	public void onResponse(CommandResponse<T> response)
 	{
 		this.manager.addWaitingResponse(response);
+	}
+
+	public Pattern getNumberPattern()
+	{
+		return this.numberPattern;
+	}
+
+	public Pattern getStringPattern()
+	{
+		return this.stringPattern;
+	}
+
+	public int getReqArgCount()
+	{
+		return this.reqArgCount;
+	}
+
+	public int getOptArgCount()
+	{
+		return this.optArgCount;
 	}
 }
---
+++ src/com/ccl/utils/Parser.java
@@ -0,0 +1,253 @@
+package com.ccl.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+
+import com.ccl.Command;
+import com.ccl.args.Arguments;
+import com.ccl.args.ProcessedArgument;
+import com.ccl.enumerations.Result;
+
+public class Parser<T, R>
+{
+
+	public Arguments processInput(Command<T, R> command, T obj, String input)
+	{
+
+		List<String> list = new ArrayList<>();
+		Matcher m = command.getStringPattern().matcher(input);
+		while (m.find())
+			list.add(m.group(1));
+
+		String[] tempArr = new String[list.size()];
+		String[] preArgs = list.toArray(tempArr);
+
+		String[] rawArgs = Arrays.copyOfRange(preArgs, 1, preArgs.length);
+
+		List<ProcessedArgument<?>> arguments = new ArrayList<>();
+
+		for (int i = 0; i < rawArgs.length; i++)
+		{
+			Matcher tm = command.getNumberPattern().matcher(rawArgs[i]);
+
+			if (!command.isShouldExecute())
+			{
+				break;
+			}
+			else if (rawArgs.length < command.parameters.size() - command.getOptArgCount() || rawArgs.length > command.parameters.size())
+			{
+				command.shutdown(obj, Result.FAILURE, "The command has an invalid number of parameters!");
+				break;
+			}
+
+			switch (command.parameters.get(i).getType())
+			{
+			case BOOLEAN:
+				if (rawArgs[i].equals("true") || rawArgs[i].equals("false") || rawArgs[i].equals("1") || rawArgs[i].equals("0"))
+				{
+					if (rawArgs[i].equals("1"))
+					{
+						rawArgs[i] = "true";
+					}
+					else if (rawArgs[i].equals("0"))
+					{
+						rawArgs[i] = "false";
+					}
+					arguments.add(new ProcessedArgument<Boolean>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Boolean.parseBoolean(rawArgs[i])));
+				}
+				else
+				{
+					command.shutdown(obj, Result.FAILURE, "Failed to parse argument " + rawArgs[i] + ", expected a boolean!");
+				}
+				break;
+			case BYTE:
+				if (tm.find() && !rawArgs[i].contains(":"))
+				{
+					byte bValue = Byte.parseByte(rawArgs[i]);
+					rawArgs[i] = MathUtils.clampb(bValue, command.parameters.get(i));
+					arguments.add(new ProcessedArgument<Byte>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Byte.parseByte(rawArgs[i])));
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					byte bValue = Byte.parseByte(split[1]);
+					arguments.add(new ProcessedArgument<Byte>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, bValue));
+				}
+				break;
+			case CHAR:
+				if (rawArgs[i].length() >= 2)
+				{
+					command.shutdown(obj, Result.FAILURE, "Failed to parse argument " + rawArgs[i] + ", expected a single character!");
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					char cValue = split[1].charAt(0);
+					arguments.add(new ProcessedArgument<Character>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, cValue));
+				}
+				else
+				{
+					arguments.add(new ProcessedArgument<Character>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], rawArgs[i].charAt(0)));
+				}
+				break;
+			case DOUBLE:
+				if (tm.find() && !rawArgs[i].contains(":"))
+				{
+					double dValue = Double.parseDouble(rawArgs[i]);
+					rawArgs[i] = MathUtils.clampd(dValue, command.parameters.get(i));
+					arguments.add(new ProcessedArgument<Double>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Double.parseDouble(rawArgs[i])));
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					double dValue = Double.parseDouble(split[1]);
+					arguments.add(new ProcessedArgument<Double>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, dValue));
+				}
+				break;
+			case FLOAT:
+				if (tm.find() && !rawArgs[i].contains(":"))
+				{
+					float fValue = Float.parseFloat(rawArgs[i]);
+					rawArgs[i] = MathUtils.clampf(fValue, command.parameters.get(i));
+					arguments.add(new ProcessedArgument<Float>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Float.parseFloat(rawArgs[i])));
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					float fValue = Float.parseFloat(split[1]);
+					arguments.add(new ProcessedArgument<Float>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, fValue));
+				}
+				break;
+			case INT:
+
+				if (tm.find() && !rawArgs[i].contains(":"))
+				{
+					int iValue = 0;
+
+					boolean neg = false;
+
+					rawArgs[i] = rawArgs[i].replaceAll("_", "").replaceAll(",", "");
+
+					if (rawArgs[i].startsWith("-"))
+					{
+						neg = true;
+						rawArgs[i] = rawArgs[i].substring(1);
+					}
+
+					if (rawArgs[i].startsWith("+"))
+					{
+						rawArgs[i] = rawArgs[i].substring(1);
+					}
+
+					if (rawArgs[i].startsWith("0b"))
+					{
+						iValue = Integer.parseInt(rawArgs[i].replace("0b", ""), 2);
+					}
+					else if (rawArgs[i].startsWith("0x"))
+					{
+						iValue = Integer.parseInt(rawArgs[i].replace("0x", ""), 16);
+					}
+					else if (rawArgs[i].startsWith("0") && rawArgs[i].length() != 1)
+					{
+						iValue = Integer.parseInt(rawArgs[i].substring(1), 8);
+					}
+					else
+					{
+						iValue = Integer.parseInt(rawArgs[i]);
+					}
+
+					if (neg)
+					{
+						iValue = 0 - iValue;
+					}
+
+					rawArgs[i] = MathUtils.clampi(iValue, command.parameters.get(i));
+					arguments.add(new ProcessedArgument<Integer>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Integer.parseInt(rawArgs[i])));
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					int iValue = Integer.parseInt(split[1]);
+					arguments.add(new ProcessedArgument<Integer>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, iValue));
+				}
+				else
+				{
+					command.shutdown(obj, Result.FAILURE, "Given parameter (" + rawArgs[i] + ") is not a valid integer value!");
+				}
+				break;
+			case LONG:
+				if (tm.find() && !rawArgs[i].contains(":"))
+				{
+					long lValue = Long.parseLong(rawArgs[i]);
+					rawArgs[i] = MathUtils.clampl(lValue, command.parameters.get(i));
+					arguments.add(new ProcessedArgument<Long>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Long.parseLong(rawArgs[i])));
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					long lValue = Long.parseLong(split[1]);
+					arguments.add(new ProcessedArgument<Long>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, lValue));
+				}
+				break;
+			case SHORT:
+				if (tm.find() && !rawArgs[i].contains(":"))
+				{
+					short sValue = Short.parseShort(rawArgs[i]);
+					rawArgs[i] = MathUtils.clamps(sValue, command.parameters.get(i));
+					arguments.add(new ProcessedArgument<Short>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], Short.parseShort(rawArgs[i])));
+				}
+				else if (rawArgs[i].contains(":"))
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					short sValue = Short.parseShort(split[1]);
+					arguments.add(new ProcessedArgument<Short>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, sValue));
+				}
+				break;
+			case STRING: // TODO: Make command not ass.
+
+				if (!rawArgs[i].contains(":"))
+				{
+					if (rawArgs[i].startsWith("\""))
+					{
+						rawArgs[i] = rawArgs[i].substring(1);
+					}
+
+					if (rawArgs[i].endsWith("\""))
+					{
+						rawArgs[i] = StringUtils.removeLastCharOptional(rawArgs[i]);
+					}
+					arguments.add(new ProcessedArgument<String>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), rawArgs[i], rawArgs[i]));
+				}
+				else
+				{
+					String[] split = rawArgs[i].split(":");
+					String tag = split[0];
+					String content = split[1];
+					if (content.startsWith("\""))
+					{
+						content = content.substring(1);
+					}
+
+					if (content.endsWith("\""))
+					{
+						content = StringUtils.removeLastCharOptional(content);
+					}
+					arguments.add(new ProcessedArgument<String>(command.parameters.get(i).getArgName(), command.parameters.get(i).getType(), tag, content));
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		return new Arguments(arguments);
+	}
+}
