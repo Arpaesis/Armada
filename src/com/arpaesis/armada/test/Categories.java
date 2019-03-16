ceee3373168a2af2675d0875981c6afff4871d29
Arpaesis <arpaesis@gmail.com>
2019-03-16 14:01:54 -0400

    Removed test commands.

--- src/com/arpaesis/armada/test/Categories.java
+++
@@ -1,9 +0,0 @@
-package com.arpaesis.armada.test;
-
-import com.arpaesis.armada.category.Category;
-
-public class Categories
-{
-	public static final Category ENTITIES = new Category();
-	public static final Category WORLD = new Category();
-}
